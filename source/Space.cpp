49eec56e5b81bce6ccd4b8603d1a92712e89fa4d
boimruczy <93208978+boimruczy@users.noreply.github.com>
2022-01-15 17:49:08 +0100

    Delete source directory

--- source/Space.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Space.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Space, "Space" );
-
-DEFINE_LOG_CATEGORY(LogSpace)
- 
